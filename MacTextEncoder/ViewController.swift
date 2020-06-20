0f002822639a7df3f29e5bed7e74436f2d112dfb
xlfdll <xlfdll@gmail.com>
2020-06-20 16:53:02 -0700

    Added source encoding combo box, and fix encoding conversion by using simple test cases

--- MacTextEncoder/ViewController.swift
+++ MacTextEncoder/ViewController.swift
@@ -14,10 +14,13 @@ class ViewController: NSViewController, NSComboBoxDelegate, NSTextFieldDelegate 
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        TargetEncodingComboBox.dataSource = self.dataSource
-        TargetEncodingComboBox.selectItem(at: self.dataSource.Encodings.firstIndex(of: String.Encoding.utf8.rawValue) ?? 0)
-        TargetEncodingComboBox.delegate = self
         SourceTextField.delegate = self
+        SourceEncodingComboBox.dataSource = self.sourceEncodingDataSource
+        SourceEncodingComboBox.selectItem(at: self.sourceEncodingDataSource.Encodings.firstIndex(of: String.Encoding.utf8.rawValue) ?? 0)
+        SourceEncodingComboBox.delegate = self
+        TargetEncodingComboBox.dataSource = self.targetEncodingDataSource
+        TargetEncodingComboBox.selectItem(at: self.targetEncodingDataSource.Encodings.firstIndex(of: String.Encoding.utf8.rawValue) ?? 0)
+        TargetEncodingComboBox.delegate = self
     }
 
     override var representedObject: Any? {
@@ -30,22 +33,25 @@ class ViewController: NSViewController, NSComboBoxDelegate, NSTextFieldDelegate 
         performTextConversion()
     }
 
-    func controlTextDidEndEditing(_ obj: Notification) {
+    func controlTextDidChange(_ notification: Notification) {
         performTextConversion()
     }
 
     private func performTextConversion() {
+        let sourceEncoding = String.Encoding(rawValue: self.sourceEncodingDataSource.Encodings[SourceEncodingComboBox.indexOfSelectedItem])
+        let targetEncoding = String.Encoding(rawValue: self.targetEncodingDataSource.Encodings[TargetEncodingComboBox.indexOfSelectedItem])
         // Get unconverted bytes using Swift's internal encoding (currently UTF-8)
         // (Can print individual bytes using indexer)
         // Refer to https://swift.org/blog/utf8-string/
-        let sourceTextData = SourceTextField.stringValue.data(using: .utf8)!
-        let targetEncoding = String.Encoding(rawValue: self.dataSource.Encodings[TargetEncodingComboBox.indexOfSelectedItem])
+        let sourceTextData = SourceTextField.stringValue.data(using: sourceEncoding, allowLossyConversion: true)!
 
-        ResultTextField.stringValue = String(data: sourceTextData, encoding: targetEncoding) ?? ""
+        ResultTextField.stringValue = String(data: sourceTextData, encoding: targetEncoding) ?? " "
     }
 
-    let dataSource = EncodingListDataSource(encodingListPointer: NSString.availableStringEncodings)
+    let sourceEncodingDataSource = EncodingListDataSource(encodingListPointer: NSString.availableStringEncodings)
+    let targetEncodingDataSource = EncodingListDataSource(encodingListPointer: NSString.availableStringEncodings)
 
+    @IBOutlet weak var SourceEncodingComboBox: NSComboBox!
     @IBOutlet weak var SourceTextField: NSTextField!
     @IBOutlet weak var TargetEncodingComboBox: NSComboBox!
     @IBOutlet weak var ResultTextField: NSTextField!
